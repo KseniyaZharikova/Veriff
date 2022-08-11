def43ae5b353a50b4f77a9704579ec0751248b00
Kseniya <kz@atommobility.com>
2022-08-11 16:33:38 +0600

    Initial Commit

---
+++ Veriff/Veriff.h
@@ -0,0 +1,18 @@
+//
+//  Veriff.h
+//  Veriff
+//
+//  Created by Kseniya Zharikova on 11/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Veriff.
+FOUNDATION_EXPORT double VeriffVersionNumber;
+
+//! Project version string for Veriff.
+FOUNDATION_EXPORT const unsigned char VeriffVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Veriff/PublicHeader.h>
+
+
